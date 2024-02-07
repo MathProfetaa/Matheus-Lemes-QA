eb77b410793d9d463cfb89733d6ea02a330a47d7
Matheus <matheus.lemes@ritain.io>
2024-02-07 14:42:41 +0000

    Message

--- enviar.php
+++ enviar.php
@@ -1,8 +1,8 @@
 <?php
 
 // Obter os dados do formulário
-$nome = $_POST['nome'];
-$email = $_POST['email'];
+$nome = $_GET['nome'];
+$email = $_GET['email'];
 
 // Exibir os dados do formulário
 echo "Nome: $nome";
